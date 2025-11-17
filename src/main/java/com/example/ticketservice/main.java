e4af74a072dac8a714605a54bab1a98e3f0e4228
user123 <144436875+Gerberg1@users.noreply.github.com>
2025-11-17 18:34:28 +0100

    Changed mail receiver and sender to functions. Added secrets.

--- src/main/java/com/example/ticketservice/main.java
+++ src/main/java/com/example/ticketservice/main.java
@@ -1,7 +1,16 @@
 package com.example.ticketservice;
 
+import entity.Mail;
+import service.EmailReceiver;
+
+import java.util.List;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("tester");
+        EmailReceiver emailReceiver = new EmailReceiver();
+        List<Mail> mails = emailReceiver.receiveMail();
+        for (Mail mail : mails){
+            System.out.println("SUBJECT: " + mail.getSubject() + " CONTENT: " + mail.getContent());
+        }
     }
 }
