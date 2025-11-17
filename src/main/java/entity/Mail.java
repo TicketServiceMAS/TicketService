2f87c218d4aa9dc31b5648a4dbd8f22c1d1a6a3a
user123 <144436875+Gerberg1@users.noreply.github.com>
2025-11-17 18:34:20 +0100

    Changed mail receiver and sender to functions. Added secrets.

---
+++ src/main/java/entity/Mail.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class Mail {
+
+    public String subject;
+    public String content;
+
+    public Mail(String subject, String content) {
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
