d52262e6d65b9d00406ec11df2a757754dfbe8f1
user123 <144436875+Gerberg1@users.noreply.github.com>
2025-11-18 13:25:13 +0100

    Added test data for everything

--- src/main/java/com/example/ticketservice/entity/Mail.java
+++ src/main/java/com/example/ticketservice/entity/Mail.java
@@ -1,28 +1,30 @@
 package com.example.ticketservice.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Mail {
 
+    public String ID;
     public String subject;
     public String content;
+
+    public Category category;
+    public Priority priority;
+    public boolean isMostImportant;
 
     public Mail(String subject, String content) {
         this.subject = subject;
         this.content = content;
     }
 
-    public String getSubject() {
-        return subject;
-    }
+    public void setID(){
+        this.ID = UUID.randomUUID().toString();
+     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
